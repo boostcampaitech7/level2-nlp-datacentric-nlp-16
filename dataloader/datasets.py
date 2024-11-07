3fc05dc60077791d1c511fe0d8ec8b345c6bee64
kangjun205 <kangjun205@gmail.com>
2024-11-07 15:23:52 +0000

    chore: Remove unused dataset

--- dataloader/datasets.py
+++ dataloader/datasets.py
@@ -2,29 +2,6 @@ import torch
 from torch.utils.data import Dataset
 
 
-class HeadlineDataset(Dataset):
-    def __init__(self, texts, labels, tokenizer, max_length=32):
-        self.texts = texts
-        self.labels = labels
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-
-    def __len__(self):
-        return len(self.texts)
-
-    def __getitem__(self, idx):
-        text = self.texts[idx]
-        label = self.labels[idx]
-
-        inputs = self.tokenizer(
-            text, padding="max_length", truncation=True, max_length=self.max_length, return_tensors="pt"
-        )
-        inputs = {key: val.squeeze() for key, val in inputs.items()}
-        label = torch.tensor(label, dtype=torch.long)
-        inputs["labels"] = label
-        return inputs
-
-
 class MaskedDataset(Dataset):
     def __init__(self, texts, labels, tokenizer, max_length=32):
         self.texts = texts
