afae5f1a07dcc21e6b4c6f9724bcee0339241cdb
kangjun205 <kangjun205@gmail.com>
2024-11-07 14:54:59 +0000

    feat: Add C-BERT model

--- dataloader/datasets.py
+++ dataloader/datasets.py
@@ -1,8 +1,9 @@
 import torch
 from torch.utils.data import Dataset
 
+
 class HeadlineDataset(Dataset):
-    def __init__(self, texts, labels, tokenizer, max_length=128):
+    def __init__(self, texts, labels, tokenizer, max_length=32):
         self.texts = texts
         self.labels = labels
         self.tokenizer = tokenizer
@@ -13,8 +14,40 @@ class HeadlineDataset(Dataset):
 
     def __getitem__(self, idx):
         text = self.texts[idx]
-        inputs = self.tokenizer(text, padding='max_length', truncation=True, max_length=self.max_length, return_tensors="pt")
+        label = self.labels[idx]
+
+        inputs = self.tokenizer(
+            text, padding="max_length", truncation=True, max_length=self.max_length, return_tensors="pt"
+        )
         inputs = {key: val.squeeze() for key, val in inputs.items()}
-        label = torch.tensor(self.labels[idx], dtype=torch.long)
-        inputs['labels'] = label
+        label = torch.tensor(label, dtype=torch.long)
+        inputs["labels"] = label
+        return inputs
+
+
+class MaskedDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length=32):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+
+        inputs = self.tokenizer(
+            text,
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors="pt",
+            return_special_tokens_mask=True,
+            return_token_type_ids=True,
+        )
+        inputs = {key: val.squeeze() for key, val in inputs.items()}
+        inputs["token_type_ids"] = [label] * len(inputs["token_type_ids"])
         return inputs
